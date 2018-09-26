ff4f3683e9c16a373af0e5991d9f6a2a02caaed6
gaaasparr <gaaasparr@gmail.com>
2018-09-26 10:38:54 +0000

    arboles agregados

--- U06_Arbol/Arbol/NodoArbol.h
+++ U06_Arbol/Arbol/NodoArbol.h
@@ -4,8 +4,47 @@
 template<class T>
 class NodoArbol {
 private:
-
+    T dato;
+    NodoArbol *izq;
+    NodoArbol *der;
 public:
+    NodoArbol() {
+        izq = nullptr;
+        der = nullptr;
+    }
+
+    NodoArbol(T d) {
+        izq = nullptr;
+        der = nullptr;
+        dato = d;
+    }
+
+    T getDato() const {
+        return dato;
+    }
+
+    void setDato(T dato) {
+        NodoArbol::dato = dato;
+    }
+
+    NodoArbol *getIzq() const {
+        return izq;
+    }
+
+    void setIzq(NodoArbol *izq) {
+        NodoArbol::izq = izq;
+    }
+
+    NodoArbol *getDer() const {
+        return der;
+    }
+
+    void setDer(NodoArbol *der) {
+        NodoArbol::der = der;
+    }
+
+
+// void actualizar(T dato){}
 
 //    void print(bool esDerecho, string identacion) {
 //        if (der != NULL) {
